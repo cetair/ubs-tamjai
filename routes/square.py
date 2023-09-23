1391c8e6788853b9fcf6ac3829bb5893122ff7a5
cetair <cathleen.trixie@gmail.com>
2023-09-23 13:06:18 +0800

    RailwayBuilder

--- routes/square.py
+++ routes/square.py
@@ -80,3 +80,39 @@ def getNextProbableWords():
 
     logging.info(result)
     return result
+
+def count_number(arr, target): 
+    if target == 0: 
+        return 1
+
+    if target < 0 or len(arr) == 0: 
+        return 0
+    
+    curr = arr[0]
+    remain = arr[1:]
+    num_curr = count_number(arr, target-curr)
+    num = count_number(remain, target)
+    return num + num_curr 
+
+@app.route('/railway-builder', methods=['POST'])
+def railwayBuilder(): 
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    input = data.get("input")
+    # input = ["5,3,2,1,4", "3,3,4,1,2", "11,1,2", "71,7,1,2,3,4,5,6,7"]
+    result = []
+    # case where target sum or diff types is 0 
+    for i in range(len(input)): 
+        temp = input[i].split(",")
+        for j in range(len(temp)):
+            temp[j] = int(temp[j])
+        if temp[0] <= 0 or temp[1] <= 0:
+            result.append(0)
+            continue 
+        else: 
+            target = temp[0]
+            temp = temp[2:]
+            res = count_number(temp, target)
+            result.append(res)
+        
+    return json.dumps(result)
